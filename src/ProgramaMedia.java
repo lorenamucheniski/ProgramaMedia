c01ff52a1ea99e424221731a25d98c27616b0fdd
lorenamucheniski <lorenamucheniski26@gmail.com>
2021-03-26 21:05:23 -0300

    firts commit

---
+++ src/ProgramaMedia.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ProgramaMedia {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite a primeira nota: ");
+        float n1 = teclado.nextFloat();
+        System.out.println("Digite a segunda nota: ");
+        float n2 = teclado.nextFloat();
+        float m = (n1+n2)/2;
+        System.out.println("Sua média é " + m);
+        if (m>9) {
+            System.out.println("Parabéns!");
+        }
+
+
+    }
+
+}
